752405c3f73d4f017d42fddda0c4d33e76cbac63
Renebert Alcarde <renebert.alcarde@ext.essilor.com>
2021-06-26 08:29:30 +0800

    -

---
+++ Objects.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlobalLib.Objects
+{
+    public class QueryResult
+    {
+        public bool Success { get; set; } = true;
+        public string Message { get; set; }
+        public string ErrorMessage { get; set; }
+        public object Data { get; set; }
+    }
+}
